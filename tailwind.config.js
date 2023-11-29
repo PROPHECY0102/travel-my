733472b767f39d9a0637f5099ef1ab28c9673f4b
PROPHECY0102 <boonkimgan@gmail.com>
2023-11-29 10:10:33 +0800

    new-nav-pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./*.{html,js}", "./kl/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./*.{html,js}",
+    "./kl/**/*.{html,js}",
+    "./explore/**/*.{html,js}",
+    "./itinerary/**/*.{html,js}",
+  ],
   theme: {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],

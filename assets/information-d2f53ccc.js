9b0f6ce9cc4c289baf878bb7dd8e8cc8d1cb690d
PROPHECY0102 <boonkimgan@gmail.com>
2023-11-30 09:46:10 +0800

    added gh-pages

---
+++ assets/information-d2f53ccc.js
@@ -0,0 +1,1 @@
+import"./headerNav-e76003f2.js";document.querySelector(".switchIMG").addEventListener("click",c);function c(a){if(a.target.classList.contains("ImageSmall1")||a.target.classList.contains("ImageSmall2")||a.target.classList.contains("ImageSmall3")){const t=a.target.src;document.querySelector(".mainImage").src=t}}
